fec0dc8000d88c809731845d4bb7da6ea3279929
sohail0992 <msohail.se@gmail.com>
2023-02-26 17:00:32 +0500

    added cors

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,7 @@ require("dotenv").config();
 import express = require("express");
 import bodyParser = require("body-parser");
 import mongoose from "mongoose";
+import cors from 'cors';
 import Email from "../models/email.model";
 const SibApiV3Sdk = require("sib-api-v3-sdk");
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
@@ -28,6 +29,8 @@ mongoose.connect(process.env.mongoUrl || "mongodb://localhost:27017/email", {
 // Create a new express application instance
 const app: express.Application = express();
 app.use(bodyParser.json());
+app.use(cors); /* NEW */
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
